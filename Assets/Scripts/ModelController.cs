cd4d84d10d01489d398539795e8c2e2b47f188a3
Taha Kızmaz <tahakizmaz@gmail.com>
2025-01-02 00:17:32 +0000

    feat(asset): Add model controller

---
+++ Assets/Scripts/ModelController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Barracuda;
+
+public class ModelController : MonoBehaviour
+{
+    [SerializeField]
+    private NNModel modelSource;
+    private IWorker worker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var model = ModelLoader.Load(modelSource);
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.Auto, model);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
